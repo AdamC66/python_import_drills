0ce61c5eb1924afd8d74d687a70bcd8de8fcbc0b
adamc66 <adam.cote66@gmail.com>
2019-07-16 13:36:59 -0400

    added import/ code to python file rather than command line

---
+++ asciiart.py
@@ -0,0 +1,12 @@
+from scene import six
+from ascii import one, two
+from art.pictures import three,four
+from art.even_more_art.images import five
+
+
+print(one())
+print(two())
+print(three())
+print(four())
+print(five())
+print(six())
